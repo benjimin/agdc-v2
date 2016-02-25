cb905174fef35f7e638250b1f72d4a54063094d4
Damien Ayers <damien@omad.net>
2016-02-25 15:05:02 +1100

    Don't lose measurement attributes

--- datacube/storage/access/core.py
+++ datacube/storage/access/core.py
@@ -190,8 +190,8 @@ class StorageUnitDimensionProxy(StorageUnitBase):
         self.coordinates.update(storage_unit.coordinates)
 
         def expand_var(var):
-            return Measurement(
-                var.dtype, var.nodata, self._dimensions + var.dimensions, var.units)
+            var.dimensions = self._dimensions + var.dimensions
+            return var
 
         self.variables = {name: expand_var(var) for name, var in storage_unit.variables.items()}
 
